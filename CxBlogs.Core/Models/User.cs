7a4c83679a02f92e993b1a891ec85a8cabf7f87c
陈城 <cc1027cc@hotmail.com>
2018-07-04 16:55:55 +0800

    移除Admin项目

---
+++ CxBlogs.Core/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using CxBlogs.Core.Framework;
+
+namespace CxBlogs.Core.Models
+{
+    public class User:FullEntity
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
